5629e10b426a69cc1b826b448222a400c3ee1b70
Zai Shi <1179961252@qq.com>
2018-03-25 10:43:41 +0200

    tune and internet

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class CarController : MonoBehaviour
+public class CarController : NetworkBehaviour
 {
-
+	[SyncVar] private Vector3 playerPos;  
+	[SyncVar] private Quaternion playerRot;  
+	[SyncVar] private float playerSpeed;  
 	public float acceleration;
 	public AnimationCurve accelerationDrag;
 	public float backAcceleration;
@@ -23,8 +26,15 @@ public class CarController : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		MoveCar ();
-		ComputeCarPhysic ();
+		if (isLocalPlayer)
+		{
+			MoveCar ();
+			ComputeCarPhysic ();
+			CmdSendServerPos (transform.position, transform.rotation, GetComponent<Rigidbody> ().velocity.magnitude);
+		} else
+		{
+			LerpPosition ();
+		} 
 	}
 
 	float InputY ()
@@ -43,19 +53,39 @@ public class CarController : MonoBehaviour
 		if (InputY () > 0)
 		{
 			GetComponent<Rigidbody> ().velocity += transform.forward * acceleration * Time.fixedDeltaTime;
-		} 
-		else if (InputY () < 0)
+		} else if (InputY () < 0)
 		{
 			GetComponent<Rigidbody> ().velocity += -transform.forward * backAcceleration * Time.fixedDeltaTime;
 		}
-		GetComponent<Rigidbody> ().AddTorque (InputX() * (Vector3.up * torque.Evaluate(GetComponent<Rigidbody> ().velocity.magnitude) * Time.fixedDeltaTime));
+
+		if (transform.InverseTransformVector(GetComponent<Rigidbody> ().velocity).z > 0)
+		{
+			GetComponent<Rigidbody> ().AddTorque (InputX () * (Vector3.up * torque.Evaluate (GetComponent<Rigidbody> ().velocity.magnitude) * Time.fixedDeltaTime));
+		} else
+		{
+			GetComponent<Rigidbody> ().AddTorque (-InputX () * (Vector3.up * torque.Evaluate (GetComponent<Rigidbody> ().velocity.magnitude) * Time.fixedDeltaTime));
+		}
 	}
 
 	void ComputeCarPhysic ()
 	{
-		GetComponent<Rigidbody> ().velocity *= (float)(1 - accelerationDrag.Evaluate(GetComponent<Rigidbody> ().velocity.x) * Time.fixedDeltaTime);
-		GetComponent<Rigidbody> ().angularVelocity *= (float)(1 - rotateDrag * Time.fixedDeltaTime);
+		GetComponent<Rigidbody> ().velocity *= (1 - accelerationDrag.Evaluate (transform.InverseTransformVector(GetComponent<Rigidbody> ().velocity).z) * Time.fixedDeltaTime);
+		GetComponent<Rigidbody> ().angularVelocity *= (1 - rotateDrag * Time.fixedDeltaTime);
 		GetComponent<Rigidbody> ().MovePosition (new Vector3 (transform.position.x, 0, transform.position.z));
-		GetComponent<Rigidbody> ().MoveRotation (Quaternion.Euler(new Vector3 (0, transform.rotation.eulerAngles.y, 0)));
+		GetComponent<Rigidbody> ().MoveRotation (Quaternion.Euler (new Vector3 (0, transform.rotation.eulerAngles.y, 0)));
 	}
+
+	void LerpPosition()  
+	{  
+		transform.position = Vector3.Lerp (transform.position, playerPos, (playerSpeed+2) * Time.fixedDeltaTime);
+		transform.rotation = Quaternion.Lerp (transform.rotation, playerRot, (playerSpeed+2) * Time.fixedDeltaTime);
+	} 
+
+	[Command]  
+	public void CmdSendServerPos(Vector3 pos, Quaternion rot, float speed)  
+	{  
+		playerPos = pos;  
+		playerRot = rot;  
+		playerSpeed = speed;
+	} 
 }
