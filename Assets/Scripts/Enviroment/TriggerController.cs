74213dbb48f6300403d608f22a63631600e50d3d
Zai Shi <1179961252@qq.com>
2018-04-18 11:03:09 +0200

    trigger

---
+++ Assets/Scripts/Enviroment/TriggerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour 
+{
+	public float hideDelay;
+	public TriggerManager manager;
+
+	void OnTriggerEnter(Collider other)
+	{
+		manager.NextTrigger ();
+		StartCoroutine ("HideTrigger", hideDelay);
+	}
+
+	private IEnumerator HideTrigger(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/Enviroment/TriggerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour {
+
+	private List<TriggerController> triggers = new List<TriggerController>();
+	private int currentTrigger = 0;
+
+	void Start () 
+	{
+		foreach (Transform child in transform)
+		{
+			child.GetComponent<TriggerController> ().manager = this;
+			triggers.Add (child.GetComponent<TriggerController>());
+			child.gameObject.SetActive (false);
+		}
+		NextTrigger ();
+	}
+
+	public void NextTrigger()
+	{
+		triggers [currentTrigger].gameObject.SetActive (true);
+		if (currentTrigger < triggers.Count - 1)
+		{
+			currentTrigger += 1;
+		} else
+		{
+			currentTrigger = 0;
+		}
+	}
+}
